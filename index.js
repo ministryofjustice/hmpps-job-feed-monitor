ce0fb7c455fcb9268c19fd66f35a166d32e55971
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-05-08 18:25:45 +0100

    Add functionality to send a notification message to Slack

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const AWS = require('aws-sdk');
 const ECS = new AWS.ECS({region: 'eu-west-2'});
+const url = require('url');
+const https = require('https');
+const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL || false;
 
 exports.handler = (event, context) => {
 	// AWS Lambda handler function
 }
 
 exports.run = async () => {
+	// Find the stopped ECS Task
 	const listTasks = await ECS.listTasks({
 		cluster: 'wp-dev',
 		family: 'hmpps-job-feed-parser-task',
@@ -17,6 +21,9 @@ exports.run = async () => {
 		process.exit(0);
 	}
 
+	// Question: do we want to report if the Task is still running?
+
+	// Check the Task's exit code
 	var describeTasks = await ECS.describeTasks({
 		cluster: 'wp-dev',
 		tasks: listTasks.taskArns
@@ -32,7 +39,60 @@ exports.run = async () => {
 	});
 }
 
-const sendError = (message) => {
-	console.error(message);
-	// Also to slack
+const sendError = async (message) => {
+	console.log('Sending message to Slack: ' + message);
+	try {
+		await postToSlack(message);
+	}
+	catch (e) {
+		console.error('ERROR: ' + e.message);
+	}
+}
+
+const postToSlack = (message) => {
+	return new Promise((resolve, reject) => {
+		if (!slackWebhookUrl) {
+			reject(new Error('Unable to post to Slack because environment variable SLACK_WEBHOOK_URL is not set'));
+		}
+
+		const postData = JSON.stringify({
+			text: message
+		});
+
+		const slackUrl = url.parse(slackWebhookUrl);
+
+		const postOptions = {
+			host: slackUrl.host,
+			path: slackUrl.pathname,
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Content-Length': Buffer.byteLength(postData)
+			}
+		};
+
+		// Set up the request
+		const request = https.request(postOptions, (response) => {
+			const body = [];
+			response.on('data', (chunk) => {
+				body.push(chunk);
+			});
+			response.on('end', () => {
+				const responseBody = body.join('');
+				if (response.statusCode < 200 || response.statusCode > 299) {
+					reject(new Error('Failed to post to Slack. Status code: ' + response.statusCode + '. Response body: ' + responseBody));
+				}
+				else {
+					resolve(responseBody);
+				}
+			});
+		});
+
+		request.on('error', (e) => {
+			reject(e);
+		});
+
+		request.write(postData);
+		request.end();
+	});
 }
